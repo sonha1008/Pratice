e48b7f87917ce66344ecdbc78633fa01f3f1e365
sonha1008 <sonnguyentrs@gmail.com>
2018-07-11 11:16:23 +0700

    send email khi add comment

--- practice03/magento3/app/code/OpenTechiz/Blog/Controller/Comment/Save.php
+++ practice03/magento3/app/code/OpenTechiz/Blog/Controller/Comment/Save.php
@@ -15,14 +15,20 @@ class Save extends Action
     protected $resultPageFactory;
     protected $resultJsonFactory;
     protected $_inlineTranslation;
+    protected $_transportBuilder;
+    protected $_scopeConfig;
 
     function __construct(
         \Magento\Framework\App\Action\Context $context,
         \OpenTechiz\Blog\Model\Post $post,
         JsonFactory $resultJsonFactory,
-        \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation
+        \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation,
+        \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
     )
     {
+        $this->_transportBuilder = $transportBuilder;
+        $this->_scopeConfig = $scopeConfig;
         $this->_resultFactory = $context->getResultFactory();
         parent::__construct($context);
         $this->_post = $post;
@@ -35,7 +41,6 @@ class Save extends Action
         $errors = false;
         $message = '';
         $post = (array) $this->getRequest()->getPostValue();
-
         if (!$post) {
             $errors = true;
             $message = "Your submission is not valid. Please try again!";  
@@ -59,7 +64,7 @@ class Save extends Action
             $result->setData(['result' => 'fail', 'message' => $message]);
         }   else {
             $message = "submit success";
-            $user_id   = $post['user_id'];
+            $user_id   = $post['comment_id'];
             $content    = $post['comment'];
             $post_id = $post['post_id'];
             $email = $post['email'];
@@ -73,6 +78,24 @@ class Save extends Action
             $comment->save();
             $result->setData(['result' => 'success', 'message' => $message]);
         }
+        $sender = [
+            'name' => 'SonHa',
+            'email' => 'php22devpro@gmail.com'
+        ];
+        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+        $transport = $this->_transportBuilder
+            ->setTemplateIdentifier($this->_scopeConfig->getValue('blog/general/template', $storeScope))
+            ->setTemplateOptions(
+                [
+                    'area' => \Magento\Framework\App\Area::AREA_FRONTEND,
+                    'store' => \Magento\Store\Model\Store::DEFAULT_STORE_ID,
+                ]
+            )
+            ->setTemplateVars(['name' => $post['comment']])
+            ->setFrom($sender)
+            ->addTo($post['email'])
+            ->getTransport()
+            ->sendMessage();
         return $result;
     }
 }
